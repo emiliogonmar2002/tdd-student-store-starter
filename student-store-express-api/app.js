7c0da6114be70f12b1c46ce9326585334f925187
Emilio Gonzalez Martinez <emiliogonmar@fb.com>
2022-06-27 20:00:30 -0700

    Initialize server, default getter

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,16 @@
-// YOUR CODE HERE
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(morgan("tiny"))
+app.use(express.json());
+
+// Get endpoint
+
+app.get("/", (_req, res) => {
+    res.status(200).json({
+        "ping" : "pong"
+    })
+})
+
+module.exports = app;
