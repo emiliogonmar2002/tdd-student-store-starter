69c84cf10c7cca9ad839799453138779852bd14d
Emilio Gonzalez Martinez <emiliogonmar@fb.com>
2022-06-27 19:54:22 -0700

    Internal Error handler

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -5,10 +5,26 @@ const app = express();
 app.use(morgan("tiny"))
 app.use(express.json());
 
+// Get endpoint
+
 app.get("/", (_req, res) => {
     res.status(200).json({
         "ping" : "pong"
     })
 })
+
+// Error handlers
+
+app.use((error, _req, res, _next) => {
+    const status = error.status || 500;
+    const message = error.message || "Something wen't wrong in the application";
+
+    return res.status(status).json({
+        error: {
+            status,
+            message
+        }
+    });
+});
 
 module.exports = app;
