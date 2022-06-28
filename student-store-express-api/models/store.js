8da2d9fb5f1bd68ccd956242b4503e82f72582bc
Emilio Gonzalez Martinez <emiliogonmar@fb.com>
2022-06-27 21:40:35 -0700

    Create Object post

--- student-store-express-api/models/store.js
+++ student-store-express-api/models/store.js
@@ -18,6 +18,47 @@ class Store {
             throw new BadRequestError(error);
         }
     }
+
+    static createOrder( {
+        shoppingCart,
+        user
+    } ) {
+        try {
+            // Check if undefined
+            if (!shoppingCart || !user) throw new BadRequestError();
+
+            // Find duplicates
+            const valuesSet = new Set(shoppingCart.map(value => value.itemId));
+            if (valuesSet.size !== shoppingCart.length) throw new BadRequestError(); // Error 400
+
+            // Calculate total
+            const total = 0;
+            shoppingCart.map(() => {
+                const { quantity, itemId } = shoppingCart[i];
+            if (!quantity || !itemId) throw new BadRequestError(); // Error 400
+
+            })
+
+            // Add taxes
+            total *= 1.0875;
+
+            // Generate purchase
+            const purchase = {
+             id: storage.get("purchases").value().length + 1,
+             name: user.name,
+             email: user.email,
+             order: shoppingCart,
+             total,
+             createdAt: new Date().toString(),
+             receipt: `Receipt #${storage.get("purchases").value().length + 1}, by user: ${user.name}. Total price: $${total}`
+            }
+
+            storage.get("purchases").push(purchase);
+            return purchase;
+        } catch (error) {
+            throw new BadRequestError(error);
+        }
+    }
 }
 
-module.exports = Store
+module.exports = Store;
