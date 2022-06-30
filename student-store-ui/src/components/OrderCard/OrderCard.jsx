f228df2f102bc3d4bc47e04d6ab12e607d256fe8
Emilio Gonzalez Martinez <emiliogonmar@fb.com>
2022-06-29 20:02:23 -0700

    Orders display

--- student-store-ui/src/components/OrderCard/OrderCard.jsx
+++ student-store-ui/src/components/OrderCard/OrderCard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./OrderCard.css";
 
-const OrderCard = () => {
-  return <div>OrderCard</div>;
+const OrderCard = (order) => {
+  return (
+    <div className="order-card">
+      <h1>{order.order.name}</h1>
+    </div>
+  );
 };
 
 export default OrderCard;
--- student-store-ui/src/components/OrdersView/OrdersView.jsx
+++ student-store-ui/src/components/OrdersView/OrdersView.jsx
@@ -1,18 +1,48 @@
-import React, { useEffect, useState } from "react";
 import "./OrdersView.css";
+import React, { useEffect, useState } from "react";
 import { API_URL } from "../../../utils/constants";
+import OrderCard from "../OrderCard/OrderCard";
 import axios from "axios";
 
-const OrdersView = (ordersAPI, setOrdersAPI) => {
+const OrdersView = () => {
+  const [ordersAPI, setOrdersAPI] = useState([]);
+  const [isLoading, setIsLoading] = useState([]);
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  const fetchOrders = async () => {
+    setIsLoading(true);
+    try {
+      const data = await axios.get(`${API_URL}/orders`);
+      setIsLoading(false);
+      if (data.status != 200) {
+        setError(data.statusText);
+      } else {
+        setOrdersAPI(data.data?.orders || []);
+      }
+    } catch (error) {
+      setIsLoading(false);
+      console.error(error);
+      setError("Server error");
+    }
+  };
+
   return (
-    <div className="orders-view">
+    <section className="orders-view">
       <h1>Orders</h1>
-      {console.log(ordersAPI)}
-      {ordersAPI &&
-        ordersAPI.ordersAPI.map((order) => {
-          <p>{order.name}</p>;
-        })}
-    </div>
+      <div className="orders-grid">
+        {ordersAPI &&
+          ordersAPI.map((order, index) => {
+            return (
+              <React.Fragment key={index}>
+                <OrderCard order={order} />
+              </React.Fragment>
+            );
+          })}
+      </div>
+    </section>
   );
 };
 
