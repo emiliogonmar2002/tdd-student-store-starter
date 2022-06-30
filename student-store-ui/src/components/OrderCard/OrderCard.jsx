1f20a0cfd86ddc9ea1e7da1fd57d14b76dc10c0a
Emilio Gonzalez Martinez <emiliogonmar@fb.com>
2022-06-29 21:16:17 -0700

    console logs erased

--- student-store-ui/src/components/OrderCard/OrderCard.jsx
+++ student-store-ui/src/components/OrderCard/OrderCard.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 
 const OrderCard = ({ order }) => (
   <Link style={{ textDecoration: "none" }} to={`/orders/${order.id}`}>
-    {console.log(order)}
     <div className="order-card">
       <h3 className="order-name">{order.name}</h3>
       <p>{order.email}</p>
